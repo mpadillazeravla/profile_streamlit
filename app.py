21a970ff8657181e72dd9bc4138028237a54d938
Miguel A. Padilla <mikepadilla82@gmail.com>
2023-07-26 10:46:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
+#from streamlit_lottie import st_lottie
 import folium
 from streamlit_folium import st_folium
 
